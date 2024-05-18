f31ed3944d4fa7d354d6ec67bc0263898ab3ea20
Konstantin Lozhkin <konstantin.l.englex@gmail.com>
2024-05-18 11:31:26 +0500

    changed getCases api

--- src/api/getCases.ts
+++ src/api/getCases.ts
@@ -1,12 +1,42 @@
-import axios from "axios"
+import axios from "axios";
+
+interface Filter {
+  Id: string;
+  Name: string
+}
+
+interface CaseResponse {
+  CaseColor: string;
+  FeaturesTitle: string;
+  Filters: Filter[];
+  FriendlyURL: string;
+  Id: string;
+  Image: string;
+  Title: string;
+}
+
+interface Response {
+  Error: string | null
+  Data:CaseResponse[]
+}
 
 export const getCases = async () => {
-  const result = await axios.get('https://services.it-cron.ru/api/cases', {
-    headers: {
-      Accept: 'text/plain',
-      AcceptLanguage: 'ru'
-    }
-  })
+  try {
+    const result = await axios.get<Response>('https://services.it-cron.ru/api/cases', {
+      headers: {
+        Accept: 'text/plain',
+        AcceptLanguage: 'ru'
+      }
+    })
 
-  console.log('result', result);
+    const cases = result.data.Data.map((item) => {
+      const Filters = item.Filters.map(filter => filter.Name);  
+      return {...item, Filters}
+    })
+
+    return cases
+  } catch (error) {
+    console.error(error)
+    return []
+  }
 }
