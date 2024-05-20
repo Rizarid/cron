2ccc339e7eea1ee17c994a9b95ff5d43746bcc15
Konstantin Lozhkin <konstantin.l.englex@gmail.com>
2024-05-20 06:26:05 +0500

    fixed font color in Case when background is white

--- src/components/Case/Case.tsx
+++ src/components/Case/Case.tsx
@@ -1,5 +1,6 @@
 import { FC, memo } from "react";
-import { CaseResult } from "../../api/getCases";
+
+import { CaseResult } from "../../api/interfaces";
 
 import styles from './Case.module.scss';
 
@@ -7,18 +8,22 @@ interface Props {
   caseProp: CaseResult
 }
 
+const WHITE = 'FFFFFF';
+
 export const Case: FC<Props> = memo(({caseProp}) => {
   const {CaseColor, FriendlyURL, Image, Title, Filters} = caseProp;
 
   const filtersList = Filters.join(', ');
+
+  const isWhiteBackground = CaseColor === WHITE;
 
   return (
     <a href={FriendlyURL}>
       <div className={styles.Case} style={{backgroundColor: `#${CaseColor}`}}>
         <img src={Image} alt={Title} className={styles.Image} />
         <div className={styles.Description}>
-          <div className={`${styles.Title} Dark`}>{Title}</div>
-          <div className={`${styles.Filters} Dark`}>{filtersList}</div>
+          <div className={`${styles.Title} ${isWhiteBackground && styles.Dark}`}>{Title}</div>
+          <div className={`${styles.Filters} ${isWhiteBackground && styles.Dark}`}>{filtersList}</div>
         </div>
       </div>
     </a>
