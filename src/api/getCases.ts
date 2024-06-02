84388425b87a3b6dedbf36a6010eb37ac2ca488d
Konstantin Lozhkin <konstantin.l.englex@gmail.com>
2024-06-02 10:53:59 +0500

    moved the api url to a constant

--- src/api/getCases.ts
+++ src/api/getCases.ts
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { CaseResult, Response } from "./interfaces";
 
+const API_URL = 'https://services.it-cron.ru/api';
+
 export const getCases = async () => {
   try {
-    const result = await axios.get<Response>('https://services.it-cron.ru/api/cases', {
+    const result = await axios.get<Response>(`${API_URL}/cases`, {
       headers: {
         Accept: 'text/plain',
         'Accept-Language': 'ru',
